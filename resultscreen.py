c8873502c8082ee5f12170f72bd9f237281a18c2
nilsonpjr <nilsonpjr@gmail.com>
2022-04-03 11:07:18 -0300

    Add files via upload

---
+++ resultscreen.py
@@ -0,0 +1,9 @@
+from kivy.uix.screenmanager import Screen
+from modulefinder import LOAD_CONST
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+
+
+class ResultScreen(MDScreen):
+
+    pass
